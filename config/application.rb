a80b73b4a9ab69205f20f6e8003244d9238edd9a
sheilahd <sheila.hdez@gmail.com>
2022-07-26 19:55:24 -0400

    config assets

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+config.assets.initialize_on_precompile = false
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -17,4 +19,3 @@ module Railsfriends
     # the framework and any gems in your application.
   end
 end
-# config.assets.initialize_on_precompile = false
